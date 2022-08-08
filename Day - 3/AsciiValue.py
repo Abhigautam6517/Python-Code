b7f2e261029d788ad0a44edbf060a671582ab5be
Abhishek Gautam <83368305+Abhigautam6517@users.noreply.github.com>
2022-08-08 11:50:16 +0530

    Add files via upload

---
+++ Day - 3/AsciiValue.py
@@ -0,0 +1,12 @@
+value=None
+while(value!="exit"):
+    value = input("Enter your Value: ");
+     
+    if(value=='exit'):
+        break;
+    else:
+        print ("The ASCII value of '" + value + "' is ", ord(value)) 
+
+
+
+     
---
+++ Day - 3/AsciiValue2.py
@@ -0,0 +1,6 @@
+# print , end = "")  
+string = input("Please enter the String: ")  
+string_length = len(string)  
+for K in string:  
+    ASCII = ord(K)  
+    print (K, "\t", ASCII)  
