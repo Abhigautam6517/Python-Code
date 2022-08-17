fafce951ea21fc5c6da79cc5f7cba66ac5d320e2
Abhishek Gautam <83368305+Abhigautam6517@users.noreply.github.com>
2022-08-17 11:52:42 +0530

    Add files via upload

---
+++ code/max_min.py
@@ -0,0 +1,12 @@
+a = [1,2,3,4,5,3,2,4,2,4,5,1,2,4]
+
+#print(max(a))
+#print(min(a))
+#print(list(reversed(a)))
+
+# print(print("some arguments"))
+print(sum(a))
+
+fun = lambda x, y : x+y
+
+print(fun(2, 4))
